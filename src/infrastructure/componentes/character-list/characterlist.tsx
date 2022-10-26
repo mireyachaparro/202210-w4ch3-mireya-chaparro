40c85f528d317980301f8118c16514f00a6e1c8c
mireyachaparro <mireyacn99@gmail.com>
2022-10-26 21:10:54 +0200

    Update fix of fix of netlify

--- src/infrastructure/componentes/character-list/characterlist.tsx
+++ src/infrastructure/componentes/character-list/characterlist.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
 import { CharacterCard } from '../character-card/charactercard';
 import { characters } from '../characters';
 
 export function CharacterList() {
-    const [initialState, setState] = useState(characters);
+    //const [initialState, setState] = useState(characters);
+    const charactersArr = [...characters];
     return (
         <>
             <div className="app container">
                 <ul className="characters-list row list-unstyled">
-                    {initialState.map((item) => (
+                    {charactersArr.map((item) => (
                         <CharacterCard character={item}></CharacterCard>
                     ))}
                 </ul>
