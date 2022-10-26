8d435407f9812d286a26557758ad1eec4d4311e2
mireyachaparro <mireyacn99@gmail.com>
2022-10-26 20:16:58 +0200

    Update funcionality components

--- src/infrastructure/componentes/character-card/charactercard.tsx
+++ src/infrastructure/componentes/character-card/charactercard.tsx
@@ -1,47 +1,59 @@
+import { useState } from 'react';
+import { characters } from '../characters';
+
 export function CharacterCard() {
+    const [initialState, setState] = useState(characters);
+
     return (
-        <li className="character col">
-            <div className="card character__card">
-                <img
-                    src="img/no-one.jpg"
-                    alt="Nombre y familia del personaje"
-                    className="character__picture card-img-top"
-                />
-                <div className="card-body">
-                    <h2 className="character__name card-title h4">
-                        Nombre y familia
-                    </h2>
-                    <div className="character__info">
-                        <ul className="list-unstyled">
-                            <li>Edad: X años</li>
-                            <li>
-                                Estado:
-                                <i className="fas fa-thumbs-down"></i>
-                                <i className="fas fa-thumbs-up"></i>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="character__overlay">
-                        <ul className="list-unstyled">
-                            <li>Años de reinado: X</li>
-                            <li>Arma: XXX</li>
-                            <li>Destreza: X</li>
-                            <li>Peloteo: X</li>
-                            <li>Asesora a: X</li>
-                            <li>Sirve a: X</li>
-                        </ul>
-                        <div className="character__actions">
-                            <button className="character__action btn">
-                                habla
-                            </button>
-                            <button className="character__action btn">
-                                muere
-                            </button>
+        <>
+            {' '}
+            <ul className="characters-list row list-unstyled">
+                {initialState.map((item) => (
+                    <li className="character col">
+                        <div className="card character__card">
+                            <img
+                                src={'./assets/' + item.name + '.jpg'}
+                                alt={item.name + item.family}
+                                className="character__picture card-img-top"
+                            />
+                            <div className="card-body">
+                                <h2 className="character__name card-title h4">
+                                    {item.name} {item.family}
+                                </h2>
+                                <div className="character__info">
+                                    <ul className="list-unstyled">
+                                        <li>Edad: {item.age} años</li>
+                                        <li>
+                                            Estado:
+                                            <i className="fas fa-thumbs-down"></i>
+                                            <i className="fas fa-thumbs-up"></i>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="character__overlay">
+                                    <ul className="list-unstyled">
+                                        <li>Años de reinado: X</li>
+                                        <li>Arma: XXX</li>
+                                        <li>Destreza: X</li>
+                                        <li>Peloteo: X</li>
+                                        <li>Asesora a: X</li>
+                                        <li>Sirve a: X</li>
+                                    </ul>
+                                    <div className="character__actions">
+                                        <button className="character__action btn">
+                                            habla
+                                        </button>
+                                        <button className="character__action btn">
+                                            muere
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <i className="emoji"></i>
                         </div>
-                    </div>
-                </div>
-                <i className="emoji"></i>
-            </div>
-        </li>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
